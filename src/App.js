39435cdcf37e087b237e6b76e1d149e22118203b
khadhraoui bilel <bilel1996k@gmail.com>
2020-02-07 17:34:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 import './App.css';
+
+
+import Navbar from './components/Navbar'
+
+
+import Home from './pages/Home';
+import Error from './pages/Error';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route excat path="/" component={Home}></Route>
+        <Route excat path="/rooms" component={Rooms}></Route>
+        <Route excat path="/rooms/:slug" component={SingleRoom}></Route>
+        <Route component={Error}></Route>
+      </Switch>
+    </Fragment>
+
   );
 }
 
